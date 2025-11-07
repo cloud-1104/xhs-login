94d25629405e540b6e9a6a8708383109e50cebb9
op <a694967190@gmail.com>
2025-11-07 14:29:10 +0800

    v0.3

--- js/login.js
+++ js/login.js
@@ -96,28 +96,28 @@ function tb(str) {
 
 
 function getxscomment(){
-    // const g = generateLocalId("Windows")
-    // const _ = "";     // 参数 x6
-    // const b = "";     // 参数 x7
-    // const merged = _ + b + g;
-    // y = {
-    // "s0": 5, //getPlatformCode传入设备平台生成
-    // "s1": "", //固定空
-    // "x0": "1", //通过localStorage算出 localStorage.getItem(u.z7)
-    // "x1": "4.2.6",
-    // "x2": "Windows",
-    // "x3": "xhs-pc-web",
-    // "x4": "4.84.4",
-    // "x5": g, //a1
-    // "x6": "",
-    // "x7": "",
-    // "x8": "I38rHdgsjopgIvesdVwgIC+oIELmBZ5e3VwXLgFTIxS3bqwErFeexd0ekncAzMFYnqthIhJeSBMDKutRI3KsYorWHPtGrbV0P9WfIi/eWc6eYqtyQApPI37ekmR6QL+5Ii6sdneeSfqYHqwl2qt5B0DBIx+PGDi/sVtkIxdsxuwr4qtiIhuaIE3e3LV0I3VTIC7e0utl2ADmsLveDSKsSPw5IEvsiVtJOqw8BuwfPpdeTFWOIx4TIiu6ZPwrPut5IvlaLbgs3qtxIxes1VwHIkumIkIyejgsY/WTge7eSqte/D7sDcpipedeYrDtIC6eDVw2IENsSqtlnlSuNjVtIvoekqt3cZ7sVo4gIESyIhE2+9DUIvzy4I8OIic7ZPwAIviC4o/sDLds6PwVIC7eSd7sd0R4IEvs3IGMtVwUIids3s/sxZNeiVtbcUeeYVwhIvMLaA7efVwRp9KsDuwmIxltIxZSouwOgVwpsr4heU/e6LveDPwFIvgs1ros1DZiIi7sjbos3grFIE0sDqwHIvmZaVtfaPwzIhOsDqwQIEAsiqwlIh/eDqtANuwPmVwDIxmxIvQpPdIRJLve3qthIEDaIk0eYuwQZbEqn00sjeHSIEYKPVwQsutaIkJeVPwDKWgskY/e6bEyIEJekd/skqtsnPwqICJeSPwiIh5eVAILIv5eYo/e0PtSICKs6PwV4omqIhHeICgeVLoeTgveYb6e1qteIEqRICTMLqw8IiSIIk5sxb832//eYW==", // localStorage.getItem(u.q2)
-    // "x9": tb(merged), //(0,p.tb)("".concat(_).concat(b).concat(g)) 需要g的值
-    // "x10": 0, //固定0
-    // "x11": "normal" //固定normal
-    // }
-    // return b64Encode(encodeUtf8( JSON.stringify(y)))
-    return '2UQAPsHC+aIjqArjwjHjNsQhPsHCH0rjNsQhPaHCH0c1Pjh9HjIj2eHjwjQgynEDJ74AHjIj2ePjwjQhyoPTqBPT49pjHjIj2ecjwjHFN0WFN0cjNsQh+aHCH0rEG/p0P0mSPeHA2BLM8fPA2nbT4emD+AL78ASh2o4E4BWFG0L7+nbj+/ZIPeZA+AL7PeDjNsQh+jHCHjHVHdW7H0ijHjIj2eWjwjQQPAYUaBzdq9k6qB4Q4fpA8b878FSet9RQzLlTcSiM8/+n4MYP8F8LagY/P9Ql4FpUzfpS2BcI8nT1GFbC/L88JdbFyrSiafp/JDMra7pFLDDAa7+8J7QgabmFz7Qjp0mcwp4fanD68p40+fp8qgzELLbILrDA+9p3JpH9LLI3+LSk+d+DJfpSL98lnLYl49IUqgcMc0mrcDShtMmozBD6qM8FyFSh8o+h4g4U+obFyLSi4nbQz/+SPFlnPrDApSzQcA4SPopFJeQmzBMA/o8Szb+NqM+c4ApQzg8Ayp8FaDRl4AYs4g4fLomD8pzBpFRQ2ezLanSM+Skc47Qc4gcMag8VGLlj87PAqgzhagYSqAbn4FYQy7pTanTQ2npx87+8NM4L89L78p+l4BL6ze4AzB+IygmS8Bp8qDzFaLP98Lzn4AQQzLEAL7bFJBEVL7pwyS8Fag868nTl4e+0n04ApfuF8FSbL7SQyrLFtASrpLS92dDFa/YOanS0+Mkc4FbQ4fSe+Bu6qFzP8oP9Lo4naLP78p+D+7+DPbHFaLp9qA+QzFMFpd4panSDqA+AN7+hnDESyp8FGf+p8np8pd4iag8+/Bbm+9pD4gqM8ASdqFzn49pQ2BlFagYyL9RM4FRdpd4Iq7HFyBppN9L9/o8Szbm7zDS987PlqfRAPLzyyLSk+7+xGfRAP94UzDSbPo+rqg4Hag8Tnfbn4B8YLo4CanYOqFzl4MbQzLbAygb7JrSiN9prqgzm/dp7LBMn4FzQ2BMhag8zqbmDapQt/o8SP7bFyrSbzBbQyAmSngp7Lpkjzgb1PemAyfpHLFSbnLTcpd4zq7pFGLS9P9ph4gzftFl68/bVcg+LqfTSanYwqAP7/9pxLozcGS8FJFDAN7+Dqg4QanSdqAbp4gSQcFTA8B8O8Lzc4b+Q2B4A2op7p0QDprRQzLI3aLP9q7YQJ9pn804S8oQOqMSc4okQypZlag8Tybkn4BRQc9lxanYdqM8P+BlHpaRA8dbF4rSi/emTqg4B2p+Oq9zQcg+xqgzk8p8F2LSe+rMQzgkEGMmFqsHVHdWEH0iTPADEweZUwerANsQhP/Zjw0ZVHdWlPaHCHfE6qfMYJsQR'
+    const g = generateLocalId("Windows")
+    const _ = "";     // 参数 x6
+    const b = "";     // 参数 x7
+    const merged = _ + b + g;
+    y = {
+    "s0": 5, //getPlatformCode传入设备平台生成
+    "s1": "", //固定空
+    "x0": "1", //通过localStorage算出 localStorage.getItem(u.z7)
+    "x1": "4.2.6",
+    "x2": "Windows",
+    "x3": "xhs-pc-web",
+    "x4": "4.84.4",
+    "x5": g, //a1
+    "x6": "",
+    "x7": "",
+    "x8": "I38rHdgsjopgIvesdVwgIC+oIELmBZ5e3VwXLgFTIxS3bqwErFeexd0ekncAzMFYnqthIhJeSBMDKutRI3KsYorWHPtGrbV0P9WfIi/eWc6eYqtyQApPI37ekmR6QL+5Ii6sdneeSfqYHqwl2qt5B0DBIx+PGDi/sVtkIxdsxuwr4qtiIhuaIE3e3LV0I3VTIC7e0utl2ADmsLveDSKsSPw5IEvsiVtJOqw8BuwfPpdeTFWOIx4TIiu6ZPwrPut5IvlaLbgs3qtxIxes1VwHIkumIkIyejgsY/WTge7eSqte/D7sDcpipedeYrDtIC6eDVw2IENsSqtlnlSuNjVtIvoekqt3cZ7sVo4gIESyIhE2+9DUIvzy4I8OIic7ZPwAIviC4o/sDLds6PwVIC7eSd7sd0R4IEvs3IGMtVwUIids3s/sxZNeiVtbcUeeYVwhIvMLaA7efVwRp9KsDuwmIxltIxZSouwOgVwpsr4heU/e6LveDPwFIvgs1ros1DZiIi7sjbos3grFIE0sDqwHIvmZaVtfaPwzIhOsDqwQIEAsiqwlIh/eDqtANuwPmVwDIxmxIvQpPdIRJLve3qthIEDaIk0eYuwQZbEqn00sjeHSIEYKPVwQsutaIkJeVPwDKWgskY/e6bEyIEJekd/skqtsnPwqICJeSPwiIh5eVAILIv5eYo/e0PtSICKs6PwV4omqIhHeICgeVLoeTgveYb6e1qteIEqRICTMLqw8IiSIIk5sxb832//eYW==", // localStorage.getItem(u.q2)
+    "x9": tb(merged), //(0,p.tb)("".concat(_).concat(b).concat(g)) 需要g的值
+    "x10": 0, //固定0
+    "x11": "normal" //固定normal
+    }
+    return b64Encode(encodeUtf8( JSON.stringify(y)))
+    // return '2UQAPsHC+aIjqArjwjHjNsQhPsHCH0rjNsQhPaHCH0c1Pjh9HjIj2eHjwjQgynEDJ74AHjIj2ePjwjQhyoPTqBPT49pjHjIj2ecjwjHFN0WFN0cjNsQh+aHCH0rEG/p0P0mSPeHA2BLM8fPA2nbT4emD+AL78ASh2o4E4BWFG0L7+nbj+/ZIPeZA+AL7PeDjNsQh+jHCHjHVHdW7H0ijHjIj2eWjwjQQPAYUaBzdq9k6qB4Q4fpA8b878FSet9RQzLlTcSiM8/+n4MYP8F8LagY/P9Ql4FpUzfpS2BcI8nT1GFbC/L88JdbFyrSiafp/JDMra7pFLDDAa7+8J7QgabmFz7Qjp0mcwp4fanD68p40+fp8qgzELLbILrDA+9p3JpH9LLI3+LSk+d+DJfpSL98lnLYl49IUqgcMc0mrcDShtMmozBD6qM8FyFSh8o+h4g4U+obFyLSi4nbQz/+SPFlnPrDApSzQcA4SPopFJeQmzBMA/o8Szb+NqM+c4ApQzg8Ayp8FaDRl4AYs4g4fLomD8pzBpFRQ2ezLanSM+Skc47Qc4gcMag8VGLlj87PAqgzhagYSqAbn4FYQy7pTanTQ2npx87+8NM4L89L78p+l4BL6ze4AzB+IygmS8Bp8qDzFaLP98Lzn4AQQzLEAL7bFJBEVL7pwyS8Fag868nTl4e+0n04ApfuF8FSbL7SQyrLFtASrpLS92dDFa/YOanS0+Mkc4FbQ4fSe+Bu6qFzP8oP9Lo4naLP78p+D+7+DPbHFaLp9qA+QzFMFpd4panSDqA+AN7+hnDESyp8FGf+p8np8pd4iag8+/Bbm+9pD4gqM8ASdqFzn49pQ2BlFagYyL9RM4FRdpd4Iq7HFyBppN9L9/o8Szbm7zDS987PlqfRAPLzyyLSk+7+xGfRAP94UzDSbPo+rqg4Hag8Tnfbn4B8YLo4CanYOqFzl4MbQzLbAygb7JrSiN9prqgzm/dp7LBMn4FzQ2BMhag8zqbmDapQt/o8SP7bFyrSbzBbQyAmSngp7Lpkjzgb1PemAyfpHLFSbnLTcpd4zq7pFGLS9P9ph4gzftFl68/bVcg+LqfTSanYwqAP7/9pxLozcGS8FJFDAN7+Dqg4QanSdqAbp4gSQcFTA8B8O8Lzc4b+Q2B4A2op7p0QDprRQzLI3aLP9q7YQJ9pn804S8oQOqMSc4okQypZlag8Tybkn4BRQc9lxanYdqM8P+BlHpaRA8dbF4rSi/emTqg4B2p+Oq9zQcg+xqgzk8p8F2LSe+rMQzgkEGMmFqsHVHdWEH0iTPADEweZUwerANsQhP/Zjw0ZVHdWlPaHCHfE6qfMYJsQR'
 }
 
 console.log(getxscomment())
